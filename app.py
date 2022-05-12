4f95409eee0ed02f6a359c71c4afa18e780efbff
PizzaMyHeart <ian_tan@hotmail.com>
2022-05-12 23:03:22 +0100

    textual - input text widget

---
+++ app.py
@@ -0,0 +1,45 @@
+from typing import Text
+from textual.app import App
+from textual.widget import Widget
+from textual.widgets import Placeholder
+from textual.reactive import Reactive
+from textual import events
+from rich.panel import Panel
+from rich.text import Text
+from rich.console import RenderableType
+from rich.align import Align
+
+class InputText(Widget):
+    title: Reactive[RenderableType] = Reactive('')
+    content: Reactive[RenderableType] = Reactive('')
+
+    def __init__(self, title: str):
+        super().__init__(title)
+        self.title = title
+
+    def on_key(self, event: events.Key) -> None:
+        if event.key == 'ctrl+h':
+            self.content = self.content[:-1]
+        else:
+            self.content += event.key
+
+    def render(self) -> RenderableType:
+        renderable = Align.left(Text(self.content, style='bold'))
+        return Panel(
+            renderable,
+            height=5,
+            title=self.title,
+            title_align='center',
+            style='bold white on rgb(50,57,50)'
+            )
+
+class Filum(App):
+    async def on_mount(self) -> None:
+        await self.view.dock(Placeholder(), edge='top', size=5)
+        await self.view.dock(InputText('url'),edge='bottom')
+        
+        
+        
+
+if __name__ == '__main__':
+    Filum.run()
--- download.py
+++ download.py
@@ -1,4 +1,3 @@
-from contextlib import nullcontext
 import requests
 from datetime import datetime
 import pprint
