39c10534bc9182373123b8893438305444a54e11
PizzaMyHeart <ian_tan@hotmail.com>
2022-06-11 22:45:33 +0100

    setup for TestDownload class

--- test/test_download.py
+++ test/test_download.py
@@ -11,28 +11,30 @@ with open(html_fp) as f:
 
 
 class TestDownload(unittest.TestCase):
+    def setUp(self):
+        self.url_reddit = 'https://www.reddit.com/r/boardgames/comments/utkslk/tiny_epic_which_one_would_you_recommend/'
+        self.url_hn = 'https://news.ycombinator.com/item?id=31707163'
+        self.url_se = 'https://stackoverflow.com/questions/46098852/best-practices-python-classes'
+
     def test_process_url_reddit(self):
-        url = 'https://www.reddit.com/r/programming/comments/2jb24/ask_reddit_unit_testing_is_great_but_how_do_you/'
-        d = Download(url)
+        d = Download(self.url_reddit)
         d.process_url()
         self.assertEqual(
             d.url,
-            'https://www.reddit.com/r/programming/comments/2jb24/ask_reddit_unit_testing_is_great_but_how_do_you/.json'
+            'https://www.reddit.com/r/boardgames/comments/utkslk/tiny_epic_which_one_would_you_recommend/.json'
             )
         self.assertEqual(d.site, 'reddit')
 
     def test_process_url_hn(self):
-        url = 'https://news.ycombinator.com/item?id=31707163'
-        d = Download(url)
+        d = Download(self.url_hn)
         d.process_url()
-        self.assertEqual(d.url, url)
+        self.assertEqual(d.url, self.url_hn)
         self.assertEqual(d.site, 'hn')
 
     def test_process_url_se(self):
-        url = 'https://stackoverflow.com/questions/46098852/best-practices-python-classes'
-        d = Download(url)
+        d = Download(self.url_se)
         d.process_url()
-        self.assertEqual(d.url, url)
+        self.assertEqual(d.url, self.url_se)
         self.assertEqual(d.site, 'se')
 
     def test_parse_html_creates_soup_from_html(self):
