b4d47f6ea1c3c584951f3b1fd52b8ceb4d8dd9ad
PizzaMyHeart <ian_tan@hotmail.com>
2022-06-11 20:30:43 +0100

    added reddit process_url test case

--- test/test_download.py
+++ test/test_download.py
@@ -11,6 +11,16 @@ with open(html_fp) as f:
 
 
 class TestDownload(unittest.TestCase):
+    def test_process_url_reddit(self):
+        url = 'https://www.reddit.com/r/programming/comments/2jb24/ask_reddit_unit_testing_is_great_but_how_do_you/'
+        d = Download(url)
+        d.process_url()
+        self.assertEqual(
+            d.url,
+            'https://www.reddit.com/r/programming/comments/2jb24/ask_reddit_unit_testing_is_great_but_how_do_you/.json'
+            )
+        self.assertEqual(d.site, 'reddit')
+
     def test_parse_html_creates_soup_from_html(self):
         d = Download('test')
         d.parse_html(html_se)
