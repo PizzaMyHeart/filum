c43484b52b862304087bcbc732455df3b1f7dce7
PizzaMyHeart <ian_tan@hotmail.com>
2022-06-13 22:44:44 +0100

    test case for number of table columns

--- test/test_view.py
+++ test/test_view.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import patch
 
 from filum.view import RichView
 
@@ -6,6 +7,22 @@ view = RichView()
 
 
 class TestView(unittest.TestCase):
+    def setUp(self) -> None:
+        self.row_dict = {
+            'row_id': 2,
+            'id': '31618955',
+            'title': 'Ask HN: Non-violent video games with great stories?',
+            'body': None,
+            'posted_timestamp': 1654336734,
+            'saved_timestamp': 1654548965.026366,
+            'score': 259,
+            'permalink': 'https://news.ycombinator.com/item?id=31618955',
+            'author': 'recvonline',
+            'source': 'hn',
+            'tags': 'games',
+            'num': 1
+            }
+
     def test_stringify(self):
         query_result = {
             'num': 1,
@@ -15,12 +32,19 @@ class TestView(unittest.TestCase):
             'score': 0,
             'source': 'hn',
             'tags': None
-            }
+        }
         result_values = query_result.values()
 
-        stringified = ('1', 'Some title', '1622404036', '1654005888.148201', '0', 'hn', 'None')
+        stringified = ('1', 'Some title', '1622404036',
+                       '1654005888.148201', '0', 'hn', 'None')
 
         self.assertEqual(view.stringify(result_values), stringified)
+
+    @patch('filum.view.Table.add_column')
+    def test_create_table_makes_seven_columns(self, mock_method):
+        row_list = [self.row_dict]
+        view.create_table(row_list)
+        self.assertEqual(mock_method.call_count, 7)
 
 
 if __name__ == '__main__':
