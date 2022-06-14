3f4b782e1590981e741b59b5fdbe460b51ca59c2
PizzaMyHeart <ian_tan@hotmail.com>
2022-06-14 18:05:21 +0100

    extracted parser class into own module

--- filum/cli.py
+++ filum/cli.py
@@ -1,8 +1,6 @@
-import argparse
 import platform
 import subprocess
 import sys
-import textwrap
 import warnings
 from cmd import Cmd
 
@@ -11,70 +9,10 @@ from rich.console import Console
 from filum.config import FilumConfig
 from filum.controller import Controller
 from filum.database import ItemAlreadyExistsError
+from filum.parser import Parser
 from filum.validation import InvalidInputError, is_valid_id, is_valid_url
 
 console = Console()
-
-
-class Parser(object):
-    parser = argparse.ArgumentParser(
-                description='Archive discussion threads',
-                prog='filum',
-                formatter_class=argparse.RawDescriptionHelpFormatter,
-                epilog=textwrap.dedent('''\
-                    Example usage:
-
-                    Add a URL
-                    $ filum add <url>
-
-                    View a table of all saved threads
-                    $ filum all
-
-                    Display a thread
-                    $ filum show <thread label>
-                    🖝 where <thread label> is the number in the left-most column of the table
-
-                    Add tags to a saved thread
-                    $ filum tags <tag 1> <tag 2> ... <tag n>
-                    🖝 add the '--delete' flag to delete these tags instead
-                    ''')
-                )
-
-    parser.add_argument('-i', action='store_true', help='interactive mode')
-
-    subparsers = parser.add_subparsers(dest='subparser')
-
-    parser_add = subparsers.add_parser('add', help='add a URL')
-    parser_add.add_argument('url', nargs='+', type=str, help='add a URL')
-    parser_add.set_defaults(parser_add=True)
-
-    parser_update = subparsers.add_parser('update', help='update a saved thread')
-    parser_update.add_argument('id', nargs=1, type=int)
-
-    parser_all = subparsers.add_parser('all', help='show all saved top-level items')
-    parser_all.set_defaults(parser_all=False)
-
-    parser_show = subparsers.add_parser('show', help='display a saved thread')
-    parser_show.add_argument('id', nargs=1, type=int)
-    parser_show.add_argument('--tags', nargs='+', help='display a thread selected from the table filtered by tags')
-    parser_show.add_argument('--source', nargs='+', help='display a thread selected from the table filtered by source')
-
-    parser_delete = subparsers.add_parser('delete', help='delete a saved thread')
-    parser_delete.add_argument('id', nargs='+', type=int)
-
-    parser_tags = subparsers.add_parser('tags', help='add tags. Include --delete to remove tags instead')
-    parser_tags.add_argument('id', nargs=1, type=int)
-    parser_tags.add_argument('tags', nargs='+', help='include one or more tags separated by a space')
-    parser_tags.add_argument('--delete', action='store_true')
-
-    parser_search = subparsers.add_parser('search', help='search for a thread')
-    parser_search.add_argument('--tags', nargs=1, help='filter table based on a tag')
-    parser_search.add_argument('--source', nargs=1, help='filter table by source')
-
-    parser_config = subparsers.add_parser('config', help='open config file')
-    parser_config.set_defaults(parser_config=False)
-
-    args = parser.parse_args()
 
 
 def main():
---
+++ filum/parser.py
@@ -0,0 +1,63 @@
+import argparse
+import textwrap
+
+
+class Parser(object):
+    parser = argparse.ArgumentParser(
+                description='Archive discussion threads',
+                prog='filum',
+                formatter_class=argparse.RawDescriptionHelpFormatter,
+                epilog=textwrap.dedent('''\
+                    Example usage:
+
+                    Add a URL
+                    $ filum add <url>
+
+                    View a table of all saved threads
+                    $ filum all
+
+                    Display a thread
+                    $ filum show <thread label>
+                    🖝 where <thread label> is the number in the left-most column of the table
+
+                    Add tags to a saved thread
+                    $ filum tags <tag 1> <tag 2> ... <tag n>
+                    🖝 add the '--delete' flag to delete these tags instead
+                    ''')
+                )
+
+    parser.add_argument('-i', action='store_true', help='interactive mode')
+
+    subparsers = parser.add_subparsers(dest='subparser')
+
+    parser_add = subparsers.add_parser('add', help='add a URL')
+    parser_add.add_argument('url', nargs='+', type=str, help='add a URL')
+    parser_add.set_defaults(parser_add=True)
+
+    parser_update = subparsers.add_parser('update', help='update a saved thread')
+    parser_update.add_argument('id', nargs=1, type=int)
+
+    parser_all = subparsers.add_parser('all', help='show all saved top-level items')
+    parser_all.set_defaults(parser_all=False)
+
+    parser_show = subparsers.add_parser('show', help='display a saved thread')
+    parser_show.add_argument('id', nargs=1, type=int)
+    parser_show.add_argument('--tags', nargs='+', help='display a thread selected from the table filtered by tags')
+    parser_show.add_argument('--source', nargs='+', help='display a thread selected from the table filtered by source')
+
+    parser_delete = subparsers.add_parser('delete', help='delete a saved thread')
+    parser_delete.add_argument('id', nargs='+', type=int)
+
+    parser_tags = subparsers.add_parser('tags', help='add tags. Include --delete to remove tags instead')
+    parser_tags.add_argument('id', nargs=1, type=int)
+    parser_tags.add_argument('tags', nargs='+', help='include one or more tags separated by a space')
+    parser_tags.add_argument('--delete', action='store_true')
+
+    parser_search = subparsers.add_parser('search', help='search for a thread')
+    parser_search.add_argument('--tags', nargs=1, help='filter table based on a tag')
+    parser_search.add_argument('--source', nargs=1, help='filter table by source')
+
+    parser_config = subparsers.add_parser('config', help='open config file')
+    parser_config.set_defaults(parser_config=False)
+
+    args = parser.parse_args()
