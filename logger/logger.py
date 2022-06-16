f53fc87b747fb991308d624f1454c376717d8e2d
PizzaMyHeart <ian_tan@hotmail.com>
2022-06-16 17:52:33 +0100

    added logger

---
+++ logger/logger.py
@@ -0,0 +1,23 @@
+import logging
+
+
+def create_logger():
+
+    # create logger
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # add formatter to ch
+    ch.setFormatter(formatter)
+
+    # add ch to logger
+    logger.addHandler(ch)
+
+    return logger
